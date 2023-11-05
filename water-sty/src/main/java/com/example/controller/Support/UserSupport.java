90eb14c2d95742d57569b6cc390ba69212c97e6b
yingithub <yin82347331@163.com>
2023-11-05 23:28:59 +0800

    用户登录 视频上传 视频播放 弹幕功能 弹幕收发 异步储存 忽略了log提交

---
+++ water-sty/src/main/java/com/example/controller/Support/UserSupport.java
@@ -0,0 +1,30 @@
+package com.example.controller.Support;
+
+import com.example.service.utils.TokenUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Author: yin7331
+ * Date: 2023/10/25 0:16
+ * Describe:
+ */
+@Component
+public class UserSupport {
+
+    public Long getCurrentUserId(){
+
+        ServletRequestAttributes current=(ServletRequestAttributes)
+                RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = current.getRequest();
+        String token = request.getHeader("token");
+        Long userId =  TokenUtil.verifyToken(token) ;
+        return userId;
+    }
+}
---
+++ water-sty/src/test/java/com/example/service/impl/ScrollingServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.example.service.impl;
+
+
+
+/**
+ * Author: yin7331
+ * Date: 2023/11/5 19:31
+ * Describe:
+ */
+public class ScrollingServiceImplTest {
+
+
+    public void saveScrollingToDB() {
+    }
+}
